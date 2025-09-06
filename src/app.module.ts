617481d597c4e65d01b2209072c028851c176bbd
lindavid1998 <lindavid1998@gmail.com>
2025-09-06 11:03:55 -0700

    Add boilerplate for transaction module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [UsersModule, PrismaModule, TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
