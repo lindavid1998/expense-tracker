cdcdf96f8c099f1bdb23c6df98280c6245b7fb25
lindavid1998 <lindavid1998@gmail.com>
2025-09-06 11:08:53 -0700

    Implement create transaction dto

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,19 @@
-export class CreateTransactionDto {}
+import { IsDate, IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTransactionDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsInt()
+  amount: number;
+
+  @IsInt()
+  userId: number;
+
+  @IsInt()
+  categoryId: number;
+
+  @IsDate()
+  timestamp?: Date;
+}
