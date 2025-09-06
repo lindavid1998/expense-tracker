c8cb10f717ac15a19116067cc313808c4b11037f
lindavid1998 <lindavid1998@gmail.com>
2025-09-06 16:31:15 -0700

    Convert expense form to react hook form for improved state management and validation

--- client/src/components/ExpenseForm.tsx
+++ client/src/components/ExpenseForm.tsx
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -9,107 +13,116 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Button } from "./ui/button";
-import { Input } from "./ui/input";
-import { Label } from "@radix-ui/react-label";
+import { Input } from "@/components/ui/input";
 import axios from "axios";
 
-/* Handle user input and send to API */
-interface ExpenseFormProps {
-  onClose?: () => void;
-}
+/* The benefit of using react hook form
+- only need to worry about:
+    defining schema, validation rules, default values
+    registering form fields to be managed by the hook
+    placeholder
+- validation, state mangement is handled by the hook
+*/
 
-interface CreateExpenseRequest {
-  amount: number;
-  categoryId: number;
-  userId: number;
-}
+const formSchema = z.object({
+  categoryId: z.coerce.number<number>().int().min(1).max(4),
+  amount: z.coerce.number<number>().int().positive(),
+});
 
-function ExpenseForm({ onClose }: ExpenseFormProps) {
-  const [amount, setAmount] = useState<number>(0);
+export default function ExpenseForm() {
+  // define the form
+  // form has properties: handleSubmit, control, formState, ...
+  // control connects form fields to state management
+  // i.e. values, dirty, onChange, validation
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      amount: 0,
+    },
+  });
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log("submit form data to api");
-    console.log(`${import.meta.env.VITE_BACKEND_HOST}/transactions`);
-
-    const data: CreateExpenseRequest = {
-      // hard coded for now
-      amount: 10,
-      userId: 1,
-      categoryId: 1,
-    };
-
+  // submit handler
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
     try {
-      await axios.post(
-        `${import.meta.env.VITE_BACKEND_HOST}/transactions`,
-        data
+      const data = {
+        amount: values.amount,
+        categoryId: values.categoryId,
+        userId: 1, // hard coded
+      };
+
+      const url = `${import.meta.env.VITE_BACKEND_HOST}/transactions`;
+      const response = await axios.post(url, data);
+      console.log(response)
+
+      console.log("Form data", values);
+      toast(
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
       );
     } catch (error) {
-      console.log(error);
+      console.error("Form submission error", error);
+      toast.error("Failed to submit the form. Please try again.");
     }
-  };
+  }
 
   return (
-    <Card className="max-w-[600px]">
-      <CardHeader>
-        <CardTitle>Add expense</CardTitle>
-        {/* <CardDescription>Card Description</CardDescription> */}
-        {/* <CardAction>Card Action</CardAction> */}
-      </CardHeader>
-      <CardContent>
-        <form onSubmit={handleSubmit}>
-          {/* amount */}
-          <Label htmlFor="amount">Amount</Label>
-          <Input
-            type="number"
-            value={amount}
-            placeholder="Enter amount"
-            id="amount"
-            // TODO: fix
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setAmount(Number(e.target.value))
-            }
-          />
+    <Form {...form}>
+      <form
+        // handleSubmit validates input before calling onSubmit
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 max-w-xl mx-auto py-10"
+      >
+        <FormField
+          control={form.control} // register the form field
+          name="amount"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Amount</FormLabel>
+              <FormControl>
+                {/* field object passes in props for value, onChange, onBlur, etc */}
+                <Input placeholder="Enter an amount" type="number" {...field} />
+              </FormControl>
 
-          {/* category selector */}
-          <Select>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Enter a category" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="1">1</SelectItem>
-              <SelectItem value="2">2</SelectItem>
-              <SelectItem value="3">3</SelectItem>
-            </SelectContent>
-          </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-          {/* actions buttons */}
-          <div className="mt-2 flex gap-2 justify-end">
-            <Button variant="secondary" type="button" onClick={onClose}>
-              Cancel
-            </Button>
-            <Button type="submit">Submit</Button>
-          </div>
-        </form>
-      </CardContent>
-    </Card>
+        <FormField
+          control={form.control}
+          name="categoryId"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Category</FormLabel>
+              <Select onValueChange={field.onChange}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a category" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="1">Food</SelectItem>
+                  <SelectItem value="2">Transportation</SelectItem>
+                  <SelectItem value="3">Entertainment</SelectItem>
+                  <SelectItem value="4">Groceries</SelectItem>
+                </SelectContent>
+              </Select>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   );
 }
-
-export default ExpenseForm;
